80f156b67e812f9b7794edcaaa5e520346747023
RIZWAN AKHTAR <riziecd@gmail.com>
2024-10-05 13:49:31 +0500

    Add files via upload
    Create required Roles and Permission with the help of  Laravel Seeder to start with the Project.

---
+++ database/seeders/UserRolePermissionSeeder.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Illuminate\Support\Facades\Hash;
+use Spatie\Permission\Models\Permission;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+class UserRolePermissionSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // Create Permissions
+        Permission::create(['name' => 'view role']);
+        Permission::create(['name' => 'create role']);
+        Permission::create(['name' => 'update role']);
+        Permission::create(['name' => 'delete role']);
+
+        Permission::create(['name' => 'view permission']);
+        Permission::create(['name' => 'create permission']);
+        Permission::create(['name' => 'update permission']);
+        Permission::create(['name' => 'delete permission']);
+
+        Permission::create(['name' => 'view user']);
+        Permission::create(['name' => 'create user']);
+        Permission::create(['name' => 'update user']);
+        Permission::create(['name' => 'delete user']);
+
+        Permission::create(['name' => 'view tasks']);
+        Permission::create(['name' => 'create tasks']);
+        Permission::create(['name' => 'update tasks']);
+        Permission::create(['name' => 'delete tasks']);
+
+
+        // Create Roles
+        $superAdminRole = Role::create(['name' => 'super-admin']); //as super-admin
+        $adminRole = Role::create(['name' => 'admin']);
+        $staffRole = Role::create(['name' => 'worker']);
+
+        // Lets give all permission to super-admin role.
+        $allPermissionNames = Permission::pluck('name')->toArray();
+
+        $superAdminRole->givePermissionTo($allPermissionNames);
+
+        // Let's give few permissions to admin role.
+        $adminRole->givePermissionTo(['create role', 'view role', 'update role']);
+        $adminRole->givePermissionTo(['create permission', 'view permission']);
+        $adminRole->givePermissionTo(['create tasks', 'view tasks', 'update tasks']);
+
+
+        // Let's Create User and assign Role to it.
+
+        $superAdminUser = User::firstOrCreate([
+                    'email' => 'abc@gmail.com',
+                ], [
+                    'name' => 'Super Admin',
+                    'email' => 'abc@gmail.com',
+                    'password' => Hash::make ('12345678'),
+                ]);
+
+        $superAdminUser->assignRole($superAdminRole);
+
+    }
+}
